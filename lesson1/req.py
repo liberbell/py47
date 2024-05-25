9d46626a9dc1721b339b12d79d4680ca77bbde7b
Liberbell <liberbell@gmail.com>
2024-05-25 10:47:08 +0900

    req4

--- lesson1/req.py
+++ lesson1/req.py
@@ -4,3 +4,6 @@ import requests
 website = "https://subslikescript.com/movie/Titanic-120338"
 
 result = requests.get(website)
+content = result.text
+soup = bs(content, "lxml")
+print(soup.prettify())
