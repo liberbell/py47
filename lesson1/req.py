5378c9ca7d3716773a81e55ab2fdff4ad5bffd9f
Liberbell <liberbell@gmail.com>
2024-05-25 11:16:17 +0900

    req9

--- lesson1/req.py
+++ lesson1/req.py
@@ -11,5 +11,5 @@ soup = bs(content, "lxml")
 box = soup.find("article", class_="main-article")
 title = box.find("h1").get_text()
 transcript = box.find("div", class_="full-script").get_text(strip=True, separator=" ")
-print(title)
-print(transcript)
+
+with
