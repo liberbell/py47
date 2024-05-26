3c583a66bbfbd2349625ff7f2910eb801efc1b23
Liberbell <liberbell@gmail.com>
2024-05-26 14:48:38 +0900

    req7

--- lesson1/req.py
+++ lesson1/req.py
@@ -19,9 +19,15 @@ for link in box.find_all("a", href=True):
 
 print(links)
 
-# box = soup.find("article", class_="main-article")
-# title = box.find("h1").get_text()
-# transcript = box.find("div", class_="full-script").get_text(strip=True, separator=" ")
+for link in links:
+    root = "https://subslikescript.com"
+    website2 = f"{root}/movies"
+    result = requests.get(website2)
+    content = result.text
+    
+    box = soup.find("article", class_="main-article")
+    title = box.find("h1").get_text()
+    transcript = box.find("div", class_="full-script").get_text(strip=True, separator=" ")
 
 # with open("titanic.txt", "w") as file:
 #     file.write(transcript)
