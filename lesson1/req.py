a830657dd4b395dd17f7fa500c0e4fdadc8442fb
Liberbell <liberbell@gmail.com>
2024-05-27 08:42:29 +0900

    req5

--- lesson1/req.py
+++ lesson1/req.py
@@ -11,34 +11,33 @@ content = result.text
 soup = bs(content, "lxml")
 # print(soup.prettify())
 
-box = soup.find("article", class_="main-article")
-links = []
-for link in box.find_all("a", href=True):
-    time.sleep(0.5)
-    links.append(link["href"])
+# box = soup.find("article", class_="main-article")
+# links = []
+# for link in box.find_all("a", href=True):
+#     time.sleep(0.5)
+#     links.append(link["href"])
 
 # print(links)
 
-for link in links:
-    website = f"{root}/{link}"
-    # print(website)
-    result = requests.get(website)
+# for link in links:
+#     website = f"{website2}/{link}"
+#     # print(website)
+#     result = requests.get(website)
+#     content = result.text
+#     soup = bs(content, "lxml")
+#     # print(content)
+
+pagination = soup.find("ul", class_="pagination")
+pages = pagination.find_all("li", class_="page-item")
+last_page = pages[-2].text
+
+for page in range(1, int(last_page) + 1):
+    website3 = f"{root}/movies_letter-A?page={page}"
+    result = requests.get(website3)
     content = result.text
     soup = bs(content, "lxml")
-    # print(content)
-
-    pagination = soup.find("ul", class_="pagination")
-    pages = pagination.find_all("li", class_="page-item")
-    last_page = pages[-2].text
-
-    for page in range(i, int(last_page) + 1):
-        website2 = f"{root}/movies_letter-A?page={page}"
-        result = requests.get(website2)
-        content = result.text
-        soup = bs(content, "lxml")
 
     box = soup.find("article", class_="main-article")
-    print(box)
 
     title = box.find("h1").get_text()
     transcript = box.find("div", class_="full-script").get_text(strip=True, separator=" ")
