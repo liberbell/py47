57b02a4c9705a50c1d31fbe20d185b32ddd2b741
Liberbell <liberbell@gmail.com>
2024-05-26 10:05:46 +0900

    req1

--- lesson1/req.py
+++ lesson1/req.py
@@ -2,12 +2,14 @@ from bs4 import BeautifulSoup as bs
 import requests
 
 website = "https://subslikescript.com/movie/Titanic-120338"
+website2 = "https://subslikescript.com/movies"
 
 result = requests.get(website)
 content = result.text
 soup = bs(content, "lxml")
 # print(soup.prettify())
 
+soup.find("article", class_="main-article")
 box = soup.find("article", class_="main-article")
 title = box.find("h1").get_text()
 transcript = box.find("div", class_="full-script").get_text(strip=True, separator=" ")
