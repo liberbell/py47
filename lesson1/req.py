0fcd1e8e2a945fd203f57e262c126cbb2aed856d
Liberbell <liberbell@gmail.com>
2024-05-26 10:08:23 +0900

    req2

--- lesson1/req.py
+++ lesson1/req.py
@@ -9,10 +9,11 @@ content = result.text
 soup = bs(content, "lxml")
 # print(soup.prettify())
 
-soup.find("article", class_="main-article")
-box = soup.find("article", class_="main-article")
-title = box.find("h1").get_text()
-transcript = box.find("div", class_="full-script").get_text(strip=True, separator=" ")
+box2 = soup.find("article", class_="main-article")
+box.find_all("a", href=True)
+# box = soup.find("article", class_="main-article")
+# title = box.find("h1").get_text()
+# transcript = box.find("div", class_="full-script").get_text(strip=True, separator=" ")
 
 with open("titanic.txt", "w") as file:
     file.write(transcript)
