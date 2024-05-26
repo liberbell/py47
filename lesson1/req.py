45060c91dcf1addb22b736da1248bb98a97efa98
Liberbell <liberbell@gmail.com>
2024-05-26 14:50:39 +0900

    req7

--- lesson1/req.py
+++ lesson1/req.py
@@ -20,12 +20,10 @@ for link in box.find_all("a", href=True):
 print(links)
 
 for link in links:
-    root = "https://subslikescript.com"
-    website2 = f"{root}/movies"
-    result = requests.get(website2)
+    movie = f"{root}/{link}"
+    result = requests.get(movie)
     content = result.text
     
-    box = soup.find("article", class_="main-article")
     title = box.find("h1").get_text()
     transcript = box.find("div", class_="full-script").get_text(strip=True, separator=" ")
 
