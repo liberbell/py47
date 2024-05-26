76742ace6659aa6f57b88a3d4afc82be044b5dbd
Liberbell <liberbell@gmail.com>
2024-05-26 14:39:28 +0900

    req4

--- lesson1/req.py
+++ lesson1/req.py
@@ -16,9 +16,11 @@ for link in box.find_all("a", href=True):
     time.sleep(1)
     links.append(link["href"])
 
+print(links)
+
 # box = soup.find("article", class_="main-article")
 # title = box.find("h1").get_text()
 # transcript = box.find("div", class_="full-script").get_text(strip=True, separator=" ")
 
-with open("titanic.txt", "w") as file:
-    file.write(transcript)
+# with open("titanic.txt", "w") as file:
+#     file.write(transcript)
