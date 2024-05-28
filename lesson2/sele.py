56aaef23e7586a3dc63fa65fc27a3cf3bfb6ce5e
Liberbell <liberbell@gmail.com>
2024-05-28 17:34:05 +0900

    sele5

--- lesson2/sele.py
+++ lesson2/sele.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
+import chromedriver_binary
 
 website = "https://www.adamchoi.co.uk/corners/detailed"
 path = "./chromedriver"
-driver = webdriver.Chrome(path)
+driver = webdriver.Chrome()
 
 driver.get(website)
