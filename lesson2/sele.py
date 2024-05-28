a60d88061c361e6ba5cb12b5e78a3adfaa78289a
Liberbell <liberbell@gmail.com>
2024-05-29 08:51:27 +0900

    sele3

--- lesson2/sele.py
+++ lesson2/sele.py
@@ -2,12 +2,13 @@ from selenium import webdriver
 import chromedriver_binary
 # pip install chromedriver-binary==125.0.6422.78.0
 
-website = "https://www.adamchoi.co.uk/corners/detailed"
+website = "https://www.adamchoi.co.uk/overs/detailed"
 # path = "./chromedriver"
 driver = webdriver.Chrome()
 
 driver.get(website)
 all_matches_button = driver.find_element_by_xpath('//label[@analytics-event="All matches"]')
+all_matches_button.click()
 
 driver.quit()
 
