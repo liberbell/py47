24d7e33ba926780df7222eef4c212f25f9cf1356
Liberbell <liberbell@gmail.com>
2024-05-30 08:26:53 +0900

    sele3

--- lesson2/sele.py
+++ lesson2/sele.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 import chromedriver_binary
 # pip install chromedriver-binary==125.0.6422.78.0
 from selenium.webdriver.common.by import By
+import pandas as pd
 
 website = "https://www.adamchoi.co.uk/overs/detailed"
 # path = "./chromedriver"
@@ -33,3 +34,10 @@ browser.quit()
 # //*[@id="page-wrapper"]/div/home-away-selector/div/div/div/div/label[2]
 # /html/body/div[2]/div/div/div[2]/div/home-away-selector/div/div/div/div/label[2]
 # <label class="btn btn-sm btn-primary ng-untouched ng-valid ng-not-empty ng-dirty active ng-valid-parse" data-ng-model="hasc.$scope.doShowSplit" data-btn-radio="false" analytics-on="click" analytics-event="All matches" analytics-category="Detailed pages filters" analytics-label="Home Away Selector">All matches</label>
+
+pd.DataFrame({
+    'date': date,
+    'home_team': home_team,
+    'score': score,
+    'away_team': away_team
+    })
