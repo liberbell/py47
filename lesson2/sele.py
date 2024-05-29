c05fd41bac0496c5ed45f1f219c143c8d2951e68
Liberbell <liberbell@gmail.com>
2024-05-29 12:59:18 +0900

    sele8

--- lesson2/sele.py
+++ lesson2/sele.py
@@ -8,6 +8,7 @@ website = "https://www.adamchoi.co.uk/overs/detailed"
 driver = webdriver.Chrome()
 
 driver.get(website)
+# all_matches_button = driver.find_element(By.XPATH, '//label[@analytics-event="All matches"]')
 all_matches_button = driver.find_element(By.XPATH, '//label[@analytics-event="All matches"]')
 all_matches_button.click()
 
@@ -18,3 +19,6 @@ for match in matches:
 driver.quit()
 
 # //*[@id="page-wrapper"]/div/home-away-selector/div/div/div/div/label[2]
+# //*[@id="page-wrapper"]/div/home-away-selector/div/div/div/div/label[2]
+# /html/body/div[2]/div/div/div[2]/div/home-away-selector/div/div/div/div/label[2]
+# <label class="btn btn-sm btn-primary ng-untouched ng-valid ng-not-empty ng-dirty active ng-valid-parse" data-ng-model="hasc.$scope.doShowSplit" data-btn-radio="false" analytics-on="click" analytics-event="All matches" analytics-category="Detailed pages filters" analytics-label="Home Away Selector">All matches</label>
