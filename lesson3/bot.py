66169e23c7ef28a468b091b31a30065434e0349c
Liberbell <liberbell@gmail.com>
2024-05-30 15:30:34 +0900

    bot2

--- lesson3/bot.py
+++ lesson3/bot.py
@@ -1,4 +1,12 @@
 from selenium import webdriver
 import chromedriver_binary
+from selenium.webdriver.common.by import By
 
 url = "https://www.audible.com/search"
+
+browser = webdriver.Chrome()
+browser.get(url)
+browser.maxmize_window()
+
+container = browser.find_element(By.CLASS_NAME, "adbl-impression-container")
+products = container.find_element(BY.XPATH, "./li")
