db9d2c5324e2d6740c541a83921d4ae67de82c42
Liberbell <liberbell@gmail.com>
2024-06-01 10:20:13 +0900

    bot7

--- lesson3/bot.py
+++ lesson3/bot.py
@@ -22,7 +22,9 @@ book_title = []
 book_author = []
 book_runtime = []
 for product in products:
-    book_title.append(product.find_element(By.XPATH, './/h3[contains(@class, "bc-heading")]').text)
+    title = product.find_element(By.XPATH, './/h3[contains(@class, "bc-heading")]').text
+    book_title.append(title)
+    print(title)
     book_author.append(product.find_element(By.XPATH, './/li[contains(@class, "authorLabel")]').text)
     book_runtime.append(product.find_element(By.XPATH, './/li[contains(@class, "runtimeLabel")]').text)
 
