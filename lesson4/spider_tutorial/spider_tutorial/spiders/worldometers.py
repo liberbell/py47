27d0088ff96595776a5f2f6db4f0e9f59b77924e
Liberbell <liberbell@gmail.com>
2024-06-03 08:59:57 +0900

    worldometers3

--- lesson4/spider_tutorial/spider_tutorial/spiders/worldometers.py
+++ lesson4/spider_tutorial/spider_tutorial/spiders/worldometers.py
@@ -7,4 +7,6 @@ class WorldometersSpider(scrapy.Spider):
     start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
 
     def parse(self, response):
-        pass
+        title = response.xpath('//h1/text()')
+
+# /html/body/div[2]/div[2]/div/div/div[1]/h1
