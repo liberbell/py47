1dd2f31fb38dfbe7da70439a264d2d87ac102a1d
Liberbell <liberbell@gmail.com>
2024-06-02 19:03:22 +0900

    bot12

--- lesson3/bot.py
+++ lesson3/bot.py
@@ -32,8 +32,8 @@ book_runtime = []
 
 while current_page <= last_page:
     time.sleep(2)
-    container = WebDriverWait
-    container = browser.find_element(By.CLASS_NAME, "adbl-impression-container")
+    container = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.CLASS_NAME, 'adbl-impression-container')))
+    # container = browser.find_element(By.CLASS_NAME, "adbl-impression-container")
     products = container.find_elements(By.XPATH, './/li[contains(@class, "productListItem")]')
 
     for product in products:
