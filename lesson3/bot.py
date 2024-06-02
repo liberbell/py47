9447c044917049d2650bbfa69c2001b3ecacf5a8
Liberbell <liberbell@gmail.com>
2024-06-02 18:51:56 +0900

    bot7

--- lesson3/bot.py
+++ lesson3/bot.py
@@ -23,13 +23,13 @@ pages = pagination.find_element(By.TAG_NAME, 'li')
 last_page = int(pages[-2].text)
 
 current_page = 1
+book_title = []
+book_author = []
+book_runtime = []
 while current_page <= last_page:
     container = browser.find_element(By.CLASS_NAME, "adbl-impression-container")
     products = container.find_elements(By.XPATH, './/li[contains(@class, "productListItem")]')
 
-    book_title = []
-    book_author = []
-    book_runtime = []
     for product in products:
         title = product.find_element(By.XPATH, './/h3[contains(@class, "bc-heading")]').text
         book_title.append(title)
@@ -37,8 +37,12 @@ while current_page <= last_page:
         book_author.append(product.find_element(By.XPATH, './/li[contains(@class, "authorLabel")]').text)
         book_runtime.append(product.find_element(By.XPATH, './/li[contains(@class, "runtimeLabel")]').text)
 
-    next_page = browser.find_element(By.XPATH, '//span[contains(@class, "nextButton")]')
-    next_page.click()
+    current_page = current_page + 1
+    try:
+        next_page = browser.find_element(By.XPATH, '//span[contains(@class, "nextButton")]')
+        next_page.click()
+    except:
+        pass
 
 # browser.quit()
 
