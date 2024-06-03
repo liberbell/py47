4831150377896a06d7992a32d638cc25c6e76770
Liberbell <liberbell@gmail.com>
2024-06-04 08:19:01 +0900

    worldometers3

--- lesson4/spider_tutorial/spider_tutorial/spiders/worldometers.py
+++ lesson4/spider_tutorial/spider_tutorial/spiders/worldometers.py
@@ -8,18 +8,18 @@ class WorldometersSpider(scrapy.Spider):
 
     def parse(self, response):
         # title = response.xpath('//h1/text()').get()
-        countries = response.xpath('//td/a')
+        countries = response.xpath("//td/a")
 
         for country in countries:
             country_name = country.xpath(".//text()").get()
-            country_url = country.xpath('.//@href').get()
-
-        yield {
-            # "countries": countries,
-            # "title": title,
-            # "countries": countries,
-            "country_name": country_name,
-            "country_url": country_url,
-        }
+            country_url = country.xpath(".//@href").get()
+            
+            yield {
+                # "countries": countries,
+                # "title": title,
+                # "countries": countries,
+                "country_name": country_name,
+                "country_url": country_url,
+            }
 
 # /html/body/div[2]/div[2]/div/div/div[1]/h1
