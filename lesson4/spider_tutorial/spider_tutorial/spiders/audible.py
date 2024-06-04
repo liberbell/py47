6923f9113f628644d37e5ac6269c24f2c3525b41
Liberbell <liberbell@gmail.com>
2024-06-05 08:34:36 +0900

    audible2

--- lesson4/spider_tutorial/spider_tutorial/spiders/audible.py
+++ lesson4/spider_tutorial/spider_tutorial/spiders/audible.py
@@ -4,7 +4,7 @@ import scrapy
 class AudibleSpider(scrapy.Spider):
     name = 'audible'
     allowed_domains = ['www.audible.com']
-    start_urls = ['http://www.audible.com/']
+    start_urls = ['https://www.audible.com/search']
 
     def parse(self, response):
         pass
