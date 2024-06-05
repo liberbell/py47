9e18f50e5860bc30891677b2924e1d52855ec334
Liberbell <liberbell@gmail.com>
2024-06-06 08:38:09 +0900

    audible8

--- lesson4/spider_tutorial/spider_tutorial/spiders/audible.py
+++ lesson4/spider_tutorial/spider_tutorial/spiders/audible.py
@@ -11,7 +11,7 @@ class AudibleSpider(scrapy.Spider):
         product_container = response.xpath('//div[@class="adbl-impression-container "]//li[contains(@class, "productListItem")]')
 
         for product in product_container:
-            book_title = product.xpath('.//h3[contains(@class, "bc-heading")]/span/a/text()').get()
+            book_title = product.xpath('.//h3[contains(@class, "bc-heading")]/a/text()').get()
             book_author = product.xpath('.//li[contains(@class, "authorLabel")]/span/a/text()').getall()
             book_length = product.xpath('.//li[contains(@class, "runtimeLabel")]/span/text()').get()
 
