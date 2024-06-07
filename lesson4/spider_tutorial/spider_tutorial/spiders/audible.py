0a704e639af052c9e3ddbc899ab7286253b3cf99
Liberbell <liberbell@gmail.com>
2024-06-07 15:35:31 +0900

    audible11

--- lesson4/spider_tutorial/spider_tutorial/spiders/audible.py
+++ lesson4/spider_tutorial/spider_tutorial/spiders/audible.py
@@ -30,5 +30,5 @@ class AudibleSpider(scrapy.Spider):
             next_page_url = pagination.xpath('.//span[contains(@class, "nextButton")]/a/@href').get()
 
             if next_page_url:
-                yield response.follow(next_page_url, callback=self.parse,
+                yield response.follow(url=next_page_url, callback=self.parse,
                                       headers={'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0'})
