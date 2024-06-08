8c8bc3e3824e89fdcd19be14c28a3fb0ff018d9c
Liberbell <liberbell@gmail.com>
2024-06-08 10:50:48 +0900

    transcripts3

--- lesson4/spider_tutorial/spider_tutorial/spiders/transcripts.py
+++ lesson4/spider_tutorial/spider_tutorial/spiders/transcripts.py
@@ -6,6 +6,9 @@ from scrapy.spiders import CrawlSpider, Rule
 class TranscriptsSpider(CrawlSpider):
     name = "transcripts"
     allowed_domains = ["subslikescript.com"]
+    custom_settings = {
+        'DOWNLOAD_DELAY': 0.5,
+    }
     start_urls = ["https://subslikescript.com/movies"]
 
     rules = (Rule(LinkExtractor(restrict_xpaths=("//ul[@class='scripts-list']/li/a")), callback="parse_item", follow=True),)
