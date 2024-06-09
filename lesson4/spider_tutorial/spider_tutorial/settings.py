32278087d5ee3df78b8ee9a903628230d0bb701a
Liberbell <liberbell@gmail.com>
2024-06-10 08:16:14 +0900

    settings1

--- lesson4/spider_tutorial/spider_tutorial/settings.py
+++ lesson4/spider_tutorial/spider_tutorial/settings.py
@@ -65,6 +65,7 @@ ROBOTSTXT_OBEY = True
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
    'spider_tutorial.pipelines.SpiderTutorialPipeline': 300,
+   'spider_tutorial.pipelines.NewAction': 200,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
