8cc0737f5a359c00988a8b110c32ac90451f7ef2
Liberbell <liberbell@gmail.com>
2024-06-13 08:25:50 +0900

    quotes3

--- lesson4/spider_tutorial/api_project/api_project/spiders/quotes.py
+++ lesson4/spider_tutorial/api_project/api_project/spiders/quotes.py
@@ -23,5 +23,5 @@ class QuotesSpider(scrapy.Spider):
             next_page_number = json_response("page") + 1
             yield scrapy.Request(
                 url = f"https://quotes.toscrape.com/api/quotes?page={next_page_number}",
-                callback=self.parse()
+                callback=self.parse
             )
