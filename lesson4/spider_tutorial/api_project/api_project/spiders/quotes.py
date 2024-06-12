7253421c2e77e4ff30208c72e854adef8fb4c3c2
Liberbell <liberbell@gmail.com>
2024-06-12 11:34:47 +0900

    quotes2

--- lesson4/spider_tutorial/api_project/api_project/spiders/quotes.py
+++ lesson4/spider_tutorial/api_project/api_project/spiders/quotes.py
@@ -4,7 +4,7 @@ import scrapy
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
     allowed_domains = ["quotes.toscrape.com"]
-    start_urls = ["https://quotes.toscrape.com"]
+    start_urls = ["https://quotes.toscrape.com/api/quotes?page=1"]
 
     def parse(self, response):
         pass
