77fc0cd088789f2b3207205382d6850b7e571134
Liberbell <liberbell@gmail.com>
2024-06-14 17:09:36 +0900

    main2

--- lesson5/main.py
+++ lesson5/main.py
@@ -3,3 +3,9 @@ import numpy as np
 import pandas as pd
 
 st.title("Streamlit introduction")
+st.write("DataFrame")
+
+df = pd.DataFrame({
+    "1stline": [1, 2, 3, 4, 5],
+    "2ndline": [10, 20, 30, 40, 50],
+})
