f65a577c33acdee049697c0833e0a9bea5ac88c4
Liberbell <liberbell@gmail.com>
2024-06-15 10:31:20 +0900

    main4

--- lesson5/main.py
+++ lesson5/main.py
@@ -6,6 +6,9 @@ from PIL import Image
 st.title("Streamlit introduction")
 st.write("Display Image")
 
+img = Image.open("aircraft.jpg")
+st.image(img, caption="aircraft", use_column_width=True)
+
 df = pd.DataFrame(
     np.random.rand(100, 2)/[50, 50] + [35.69, 139.70],
     columns = ["lat", "lon"],
