8a49617fd189e014c7059e1bf8a597255adffecc
Liberbell <liberbell@gmail.com>
2024-06-17 08:10:36 +0900

    stock_price2

--- lesson6/stock_price.py
+++ lesson6/stock_price.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 import yfinance as yf
 
 appl = yf.Ticker("AAPL")
-days = 20
+days = 5
+input_date = str(days) + "d"
 # print(appl.history(period=f'{days}d'))
-hist = appl.history(period=f'{days}d')
+hist = appl.history(period=input_date)
 print(hist)
