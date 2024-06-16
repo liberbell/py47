32e11599496c38fa5afe1a3be14f7000852b2333
Liberbell <liberbell@gmail.com>
2024-06-16 12:25:09 +0900

    main7

--- lesson5/main.py
+++ lesson5/main.py
@@ -11,7 +11,10 @@ st.write("Show Progress Bar")
 latest_iteration = st.empty()
 bar = st.progress(0)
 for i in range(100):
+    latest_iteration.text(f"Iteration {i+1}")
+    bar.progress(i+1)
     time.sleep(0.1)
+
 
 left_column, right_column = st.columns(2)
 button = left_column.button("Show Right Column")
