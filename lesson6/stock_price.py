74c116fcab2ecabe360d6240eca1ada2804de904
Liberbell <liberbell@gmail.com>
2024-06-17 08:47:28 +0900

    stock_price10

--- lesson6/stock_price.py
+++ lesson6/stock_price.py
@@ -7,16 +7,17 @@ days = 1
 input_date = str(days) + "mo"
 tickers = {
     "apple": "AAPL",
-    "facebook": "FB"
+    "facebook": "Meta"
 }
 # print(appl.history(period=f'{days}d'))
-company = "apple"
-stock_hist = yf.Ticker(tickers[company])
-hist = stock_hist.history(period=input_date)
+for company in tickers.keys():
+    stock_hist = yf.Ticker(tickers[company])
+    hist = stock_hist.history(period=input_date)
 
-hist.index = hist.index.strftime("%d %B %Y")
-hist = hist[['Close']]
-hist.columns = [company]
-hist = hist.T
-hist.index.name = "Name"
-print(hist)
+    hist.index = hist.index.strftime("%d %B %Y")
+    hist = hist[['Close']]
+    hist.columns = [company]
+    hist = hist.T
+    hist.index.name = "Name"
+
+
