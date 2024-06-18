5b7dec11487a4cb357b93800a4b83cd137f6638c
Liberbell <liberbell@gmail.com>
2024-06-19 08:51:55 +0900

    ai_api2

--- lesson7/ai_api.py
+++ lesson7/ai_api.py
@@ -4,6 +4,7 @@ config_ini = configparser.ConfigParser()
 config_ini.read('auth.ini', encoding='utf-8')
 
 user = config_ini['DEFAULT']['User']
+endpoint = config_ini['DEFAULT']['Endpoint']
 auth_key = config_ini['DEFAULT']['Auth_key']
 
-print(user, auth_key)
+print(endpoint, auth_key)
