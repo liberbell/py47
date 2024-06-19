05988a386a4f720c42d51c13cec03de067e4ea52
Liberbell <liberbell@gmail.com>
2024-06-19 10:51:31 +0900

    ai_api10

--- lesson7/ai_api.py
+++ lesson7/ai_api.py
@@ -20,19 +20,19 @@ client = ImageAnalysisClient(
     credential=AzureKeyCredential(key)
 )
 
-result = client.analyze_from_url(
-    image_url="https://learn.microsoft.com/azure/ai-services/computer-vision/media/quickstarts/presentation.png",
-    visual_features=[VisualFeatures.CAPTION, VisualFeatures.READ],
-    gender_neutral_caption=True,  # Optional (default is False)
-)
+# result = client.analyze_from_url(
+#     image_url="https://learn.microsoft.com/azure/ai-services/computer-vision/media/quickstarts/presentation.png",
+#     visual_features=[VisualFeatures.CAPTION, VisualFeatures.READ],
+#     gender_neutral_caption=True,
+# )
 
-print(" Caption:")
-if result.caption is not None:
-    print(f"   '{result.caption.text}', Confidence {result.caption.confidence:.4f}")
+# print(" Caption:")
+# if result.caption is not None:
+#     print(f"   '{result.caption.text}', Confidence {result.caption.confidence:.4f}")
 
-print(" Read:")
-if result.read is not None:
-    for line in result.read.blocks[0].lines:
-        print(f"   Line: '{line.text}', Bounding box {line.bounding_polygon}")
-        for word in line.words:
-            print(f"     Word: '{word.text}', Bounding polygon {word.bounding_polygon}, Confidence {word.confidence:.4f}")
+# print(" Read:")
+# if result.read is not None:
+#     for line in result.read.blocks[0].lines:
+#         print(f"   Line: '{line.text}', Bounding box {line.bounding_polygon}")
+#         for word in line.words:
+#             print(f"     Word: '{word.text}', Bounding polygon {word.bounding_polygon}, Confidence {word.confidence:.4f}")
