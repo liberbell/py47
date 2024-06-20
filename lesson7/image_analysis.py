6b34c3034f628b7bfa131b448e0fa6439e25ca3d
Liberbell <liberbell@gmail.com>
2024-06-21 08:36:50 +0900

    image_analysis1

---
+++ lesson7/image_analysis.py
@@ -0,0 +1,43 @@
+import configparser
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
+from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
+from msrest.authentication import CognitiveServicesCredentials
+
+from array import array
+import os
+from PIL import Image
+import sys
+import time
+
+try:
+    config_ini = configparser.ConfigParser()
+    config_ini.read('auth.ini', encoding='utf-8')
+
+    endpoint = config_ini['DEFAULT']['Endpoint']
+    subscription_key = config_ini['DEFAULT']['Auth_key']
+    # print(endpoint, key)
+except KeyError:
+    print("Missing environment variable 'VISION_ENDPOINT' or 'VISION_KEY'")
+    print("Set them before running this sample.")
+    exit()
+
+computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))
+
+images_folder = os.path.join (os.path.dirname(os.path.abspath(__file__)), "images")
+remote_image_url = "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-sample-data-files/master/ComputerVision/Images/landmark.jpg"
+
+print("===== Tag an image - remote =====")
+# Call API with remote image
+tags_result_remote = computervision_client.tag_image(remote_image_url)
+
+# Print results with confidence score
+print("Tags in the remote image: ")
+if (len(tags_result_remote.tags) == 0):
+    print("No tags detected.")
+else:
+    for tag in tags_result_remote.tags:
+        print("'{}' with confidence {:.2f}%".format(tag.name, tag.confidence * 100))
+print()
+
+print("End of Computer Vision quickstart.")
