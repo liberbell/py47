2412b7ca16142977bd1a1cfa98b235aff1753bf6
Liberbell <liberbell@gmail.com>
2024-06-25 08:56:50 +0900

    texttospeech10

--- lesson8/texttospeech.py
+++ lesson8/texttospeech.py
@@ -4,25 +4,25 @@ import pygame
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "secret.json"
 
-client = texttospeech.TextToSpeechClient()
-synthesis_input = texttospeech.SynthesisInput(text="私がチャンピオンだ。")
+# client = texttospeech.TextToSpeechClient()
+# synthesis_input = texttospeech.SynthesisInput(text="私がチャンピオンだ。")
 
-voice = texttospeech.VoiceSelectionParams(
-    language_code="ja-JP", ssml_gender=texttospeech.SsmlVoiceGender.MALE
-)
+# voice = texttospeech.VoiceSelectionParams(
+#     language_code="ja-JP", ssml_gender=texttospeech.SsmlVoiceGender.MALE
+# )
 
-audio_config = texttospeech.AudioConfig(
-    audio_encoding=texttospeech.AudioEncoding.MP3
-)
+# audio_config = texttospeech.AudioConfig(
+#     audio_encoding=texttospeech.AudioEncoding.MP3
+# )
 
-response = client.synthesize_speech(
-    input=synthesis_input, voice=voice, audio_config=audio_config
-)
+# response = client.synthesize_speech(
+#     input=synthesis_input, voice=voice, audio_config=audio_config
+# )
 
-filename = "output2.mp3"
-with open(filename, "wb") as out:
-    out.write(response.audio_content)
-    print(f'Audio content written to file "{filename}"')
+# filename = "output2.mp3"
+# with open(filename, "wb") as out:
+#     out.write(response.audio_content)
+#     print(f'Audio content written to file "{filename}"')
 
 pygame.mixer.init()
 pygame.mixer.music.load(filename)
@@ -34,3 +34,18 @@ gender_type = {
     "female": texttospeech.SsmlVoiceGender.FEMALE,
     "neutral": texttospeech.SsmlVoiceGender.NEUTRAL,
 }
+gender = "default"
+client = texttospeech.TextToSpeechClient()
+synthesis_input = texttospeech.SynthesisInput(text="私がチャンピオンだ。")
+
+voice = texttospeech.VoiceSelectionParams(
+    language_code="ja-JP", ssml_gender=gender
+)
+
+audio_config = texttospeech.AudioConfig(
+    audio_encoding=texttospeech.AudioEncoding.MP3
+)
+
+response = client.synthesize_speech(
+    input=synthesis_input, voice=voice, audio_config=audio_config
+)
