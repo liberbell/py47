f459d5ac478e361ceb06bc938f23be57e135b05c
Liberbell <liberbell@gmail.com>
2024-06-28 09:41:07 +0900

    youtube_api3

--- lesson9/youtube_api.py
+++ lesson9/youtube_api.py
@@ -7,8 +7,6 @@ import googleapiclient.errors
 scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 
 def main():
-    # Disable OAuthlib's HTTPS verification when running locally.
-    # *DO NOT* leave this option enabled in production.
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
     api_service_name = "youtube"
