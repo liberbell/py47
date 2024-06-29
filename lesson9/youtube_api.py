7bc1bcdb6572c67c7e495b5d84bcf5276795d5d9
Liberbell <liberbell@gmail.com>
2024-06-29 20:12:39 +0900

    youtube_api2

--- lesson9/youtube_api.py
+++ lesson9/youtube_api.py
@@ -4,7 +4,7 @@ import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
 
-# scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 
 def main():
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
@@ -15,7 +15,7 @@ def main():
 
     # Get credentials and create an API client
     flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file)
+        client_secrets_file, scopes=scopes)
     credentials = flow.run_console()
     youtube = googleapiclient.discovery.build(
         api_service_name, api_version, credentials=credentials)
