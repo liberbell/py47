cfd49ef50b3b008666ef41c48295c302ed1a994a
Liberbell <liberbell@gmail.com>
2024-06-30 13:58:04 +0900

    youtube_api23

--- lesson9/youtube_api2.py
+++ lesson9/youtube_api2.py
@@ -14,15 +14,6 @@ youtube = build(api_service_name, api_version, developerKey=api_developer_key)
 q = "Python"
 max_result = 30
 
-request = youtube.search().list(
-    part="id,snippet",
-    maxResults=max_result,
-    q=q,
-    order="viewCount",
-    type="video"
-)
-response = request.execute()
-
 items = response["items"]
 items_id = []
 for item in items:
@@ -30,6 +21,17 @@ for item in items:
     item_id["video_id"] = item["id"]["videoId"]
     item_id["channel_id"] = item["snippet"]["channelId"]
     items_id.append(item_id)
+
+
+def search_video(youtube, q="automate", max_result=50):
+    request = youtube.search().list(
+        part="id,snippet",
+        maxResults=max_result,
+        q=q,
+        order="viewCount",
+        type="video"
+    )
+    response = request.execute()
 
 # print(items_id)
 
