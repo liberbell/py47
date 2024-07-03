3b8fdf22af73a64415238efa312a777aad20f832
Liberbell <liberbell@gmail.com>
2024-07-04 08:21:49 +0900

    web_scraping3

--- lesson10/web_scraping.py
+++ lesson10/web_scraping.py
@@ -3,3 +3,7 @@ import requests
 
 url = "https://scraping-for-beginner.herokuapp.com/udemy"
 web_data = requests.get(url)
+
+# print(web_data.text)
+result = bs(web_data.text, "html.parser")
+print(result)
