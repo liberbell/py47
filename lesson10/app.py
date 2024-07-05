c5136e3b8d95c8760b427a2b3fe6c2ff6d51dae3
Liberbell <liberbell@gmail.com>
2024-07-05 15:07:50 +0900

    app1

---
+++ lesson10/app.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup as bs
+import requests
+
+data_udemy = {}
+
+url = "https://scraping-for-beginner.herokuapp.com/udemy"
+web_data = requests.get(url)
+result = bs(web_data.text, "html.parser")
+
+n_subscriber = result.find('p', {'class': 'subscribers'}).text
+n_subscriber = int(n_subscriber.split("：")[1])
+n_review = result.find('p', {'class': 'reviews'}).text
+n_review =int(n_review.split("：")[1])
