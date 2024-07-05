5a745d2c3194951fe6dbd65d39e73375c468bdc8
Liberbell <liberbell@gmail.com>
2024-07-05 15:09:22 +0900

    app2

--- lesson10/app.py
+++ lesson10/app.py
@@ -3,11 +3,16 @@ import requests
 
 data_udemy = {}
 
-url = "https://scraping-for-beginner.herokuapp.com/udemy"
-web_data = requests.get(url)
-result = bs(web_data.text, "html.parser")
+def get_data_udemy():
+    url = "https://scraping-for-beginner.herokuapp.com/udemy"
+    web_data = requests.get(url)
+    result = bs(web_data.text, "html.parser")
 
-n_subscriber = result.find('p', {'class': 'subscribers'}).text
-n_subscriber = int(n_subscriber.split("：")[1])
-n_review = result.find('p', {'class': 'reviews'}).text
-n_review =int(n_review.split("：")[1])
+    n_subscriber = result.find('p', {'class': 'subscribers'}).text
+    n_subscriber = int(n_subscriber.split("：")[1])
+    n_review = result.find('p', {'class': 'reviews'}).text
+    n_review =int(n_review.split("：")[1])
+    return {
+        "n_subscriber": n_subscriber,
+        "n_review": n_review
+    }
