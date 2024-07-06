f87ba5f24a9b431d2f3b910a348a063951d498f0
Liberbell <liberbell@gmail.com>
2024-07-06 10:46:02 +0900

    spreadsheet5

--- lesson10/spreadsheet.py
+++ lesson10/spreadsheet.py
@@ -13,7 +13,6 @@ credentials = Credentials.from_service_account_file(
 
 gc = gspread.authorize(credentials)
 
-SP_SHEET_KEY = "1C-278Mp-exInLJ1jpkaienNeAjXUNRGmEI_-T5jSH10"
-# https://docs.google.com/spreadsheets/d/1C-278Mp-exInLJ1jpkaienNeAjXUNRGmEI_-T5jSH10/edit?usp=share_link
+SP_SHEET_KEY = '1C-278Mp-exInLJ1jpkaienNeAjXUNRGmEI_-T5jSH10'
 
 sh = gc.open_by_key(SP_SHEET_KEY)
