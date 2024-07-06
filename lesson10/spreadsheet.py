fe05add130d2545215db97b1ac5bfb0c45ef2059
Liberbell <liberbell@gmail.com>
2024-07-06 11:00:29 +0900

    spreadsheet10

--- lesson10/spreadsheet.py
+++ lesson10/spreadsheet.py
@@ -6,12 +6,10 @@ scopes = [
     'https://www.googleapis.com/auth/spreadsheets',
     'https://www.googleapis.com/auth/drive'
 ]
-
 credentials = Credentials.from_service_account_file(
     'service_account.json',
     scopes=scopes
 )
-
 gc = gspread.authorize(credentials)
 
 SP_SHEET_KEY = '1C-278Mp-exInLJ1jpkaienNeAjXUNRGmEI_-T5jSH10'
