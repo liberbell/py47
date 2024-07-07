c0a46c4bfca11b9750c9f3ff4a387401c3d81370
Liberbell <liberbell@gmail.com>
2024-07-08 08:27:38 +0900

    app8

--- lesson10/app.py
+++ lesson10/app.py
@@ -83,27 +83,36 @@ set_with_dataframe(worksheet, df, row=1, col=1)
 data = worksheet.get_all_values()
 df_udemy = pd.DataFrame(data[1:], columns=data[0])
 
+df_udemy = df_udemy.astype({
+    "n_subscriber": int,
+    "n_review": int
+})
+print(df_udemy.dtypes)
+ymin1 = df_udemy["n_subscriber"].min()-10
+ymax1 = df_udemy["n_subscriber"].min()+10
+
+ymin2 = df_udemy["n_review"].min()-10
+ymax2 = df_udemy["n_review"].min()+10
+
 base = alt.Chart(df_udemy).encode(
     alt.X('date:T', axis=alt.Axis(title=None))
 )
 
 line1 = base.mark_line(opacity=0.3, color='#57A44C').encode(
     alt.Y('n_subscriber',
-          axis=alt.Axis(title="Subscriber number", titleColor='#57A44C'))
+          axis=alt.Axis(title="Subscriber number", titleColor='#57A44C'),
+          scale=alt.Scale(domain=[ymin1, ymax1])
+          )
 )
 
 line2 = base.mark_line(stroke='#5276A7', interpolate='monotone').encode(
     alt.Y("n_review",
-          axis=alt.Axis(title="Reviewers", titleColor='#5276A7'))
+          axis=alt.Axis(title="Reviewers", titleColor='#5276A7'),
+          scale=alt.Scale([ymin2, ymax2])
+          )
 )
 
 chart = alt.layer(line1, line2).resolve_scale(
     y='independent'
 )
 
-df_udemy = df_udemy.astype({
-    "n_subscriber": int,
-    "n_review": int
-})
-print(df_udemy.dtypes)
-print(int(df_udemy["n_subscriber"].min())-10)
