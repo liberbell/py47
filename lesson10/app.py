6c9044e2a30837d57f3f9ddd4e15eb553117a377
Liberbell <liberbell@gmail.com>
2024-07-09 08:14:35 +0900

    app2

--- lesson10/app.py
+++ lesson10/app.py
@@ -67,7 +67,35 @@ def get_worksheet():
 
 worksheet = get_worksheet()
 data = worksheet.get_all_values()
-df = pd.DataFrame(data[1:], columns=data[0])
+df_udemy = pd.DataFrame(data[1:], columns=data[0])
+
+df_udemy = df_udemy.astype({
+    "n_subscriber": int,
+    "n_review": int
+})
+ymin1 = df_udemy["n_subscriber"].min()-10
+ymax1 = df_udemy["n_subscriber"].min()+10
+ymin2 = df_udemy["n_review"].min()-10
+ymax2 = df_udemy["n_review"].min()+10
+
+base = alt.Chart(df_udemy).encode(
+    alt.X('date:T', axis=alt.Axis(title=None))
+)
+
+line1 = base.mark_line(opacity=0.3, color='#57A44C').encode(
+    alt.Y('n_subscriber',
+          axis=alt.Axis(title="Subscriber number", titleColor='#57A44C'),
+          scale=alt.Scale(domain=[ymin1, ymax1])
+          )
+)
+
+line2 = base.mark_line(stroke='#5276A7', interpolate='monotone').encode(
+    alt.Y("n_review",
+          axis=alt.Axis(title="Reviewers", titleColor='#5276A7'),
+          scale=alt.Scale(domain=[ymin2, ymax2])
+          )
+)
+
 
 data_udemy = get_data_udemy()
 
