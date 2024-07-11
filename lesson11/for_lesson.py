d430a900e9934db32a307c44cea0491187b078f4
Liberbell <liberbell@gmail.com>
2024-07-11 09:07:03 +0900

    for_lesson3

--- lesson11/for_lesson.py
+++ lesson11/for_lesson.py
@@ -1,12 +1,21 @@
 names = ["bob", "alex", "eric", "ed", "aletha"]
-print(names[0] + " san")
-print(names[1] + " san")
-print(names[2] + " san")
-print(names[3] + " san")
-print(names[4] + " san")
+# print(names[0] + " san")
+# print(names[1] + " san")
+# print(names[2] + " san")
+# print(names[3] + " san")
+# print(names[4] + " san")
 
-for i in range(len(names)):
-    print(names[i] + " san")
+# for i in range(len(names)):
+#     print(names[i] + " san")
 
-for name in names:
-    print(name + " san")
+# for name in names:
+#     print(name + " san")
+
+last_names = ["Clapton", "Mary", "Denvor", "Lenon", "Sheeran"]
+first_name = ["Bob", "John", "Ed", "Eric", "Alex"]
+
+print(last_names[0] + first_name[0] + " san")
+print(last_names[1] + first_name[1] + " san")
+print(last_names[2] + first_name[2] + " san")
+print(last_names[3] + first_name[3] + " san")
+print(last_names[4] + first_name[4] + " san")
