3f7de7d6f9600ba896d253596cad83d4a56cfe31
Liberbell <liberbell@gmail.com>
2024-07-11 09:09:48 +0900

    for_lesson5

--- lesson11/for_lesson.py
+++ lesson11/for_lesson.py
@@ -12,13 +12,16 @@ names = ["bob", "alex", "eric", "ed", "aletha"]
 #     print(name + " san")
 
 last_names = ["Clapton", "Mary", "Denvor", "Lenon", "Sheeran"]
-first_name = ["Bob", "John", "Ed", "Eric", "Alex"]
+first_names = ["Bob", "John", "Ed", "Eric", "Alex"]
 
-print(last_names[0] + first_name[0] + " san")
-print(last_names[1] + first_name[1] + " san")
-print(last_names[2] + first_name[2] + " san")
-print(last_names[3] + first_name[3] + " san")
-print(last_names[4] + first_name[4] + " san")
+print(last_names[0] + first_names[0] + " san")
+print(last_names[1] + first_names[1] + " san")
+print(last_names[2] + first_names[2] + " san")
+print(last_names[3] + first_names[3] + " san")
+print(last_names[4] + first_names[4] + " san")
 
 for i in range(len(last_names)):
-    print(last_names[i] + first_name[i] + " san")
+    print(last_names[i] + first_names[i] + " san")
+
+for last_name, first_name in zip(last_names, first_names):
+    print(last_name, first_name + " san")
