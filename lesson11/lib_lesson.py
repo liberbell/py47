89033f57eba33afea33019eb33387ee0ae481d29
Liberbell <liberbell@gmail.com>
2024-07-13 23:12:52 +0900

    lib_lesson3

--- lesson11/lib_lesson.py
+++ lesson11/lib_lesson.py
@@ -2,4 +2,6 @@ import datetime
 
 today = datetime.date.today()
 print(today.year, today.month, today.day)
+yesterday = today - datetime.timedelta(days=1)
+print(yesterday)
 print(datetime.datetime.now())
