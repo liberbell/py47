8875c2ee19c61437aaa42058669e36d130c63480
Liberbell <liberbell@gmail.com>
2024-07-13 23:19:17 +0900

    lib_lesson5

--- lesson11/lib_lesson.py
+++ lesson11/lib_lesson.py
@@ -2,10 +2,11 @@ import datetime
 import pandas as pd
 
 today = datetime.date.today()
-print(today.year, today.month, today.day)
+# print(today.year, today.month, today.day)
 yesterday = today - datetime.timedelta(days=1)
-print(yesterday)
-print(today.strftime("%Y/%m/%d"))
-print(datetime.datetime.now())
+# print(yesterday)
+# print(today.strftime("%Y/%m/%d"))
+# print(datetime.datetime.now())
 
-pd.DataFrame([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+df = pd.DataFrame([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(df)
